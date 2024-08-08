172676bc51e94f73fe40f8492d340ec97787f8e8
seophohoho <seop0937@gmail.com>
2024-08-08 21:25:28 +0900

    delete incorrectly uploaded files

--- account/src/main/java/com/ckeeper/account/entity/AccountEntity.java
+++
@@ -1,8 +0,0 @@
-package com.ckeeper.account.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class AccountEntity {
-
-}
--- account/src/main/java/com/ckeeper/account/repository/RegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ckeeper.account.repository;
-
-import com.ckeeper.account.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<AccountEntity, Long> {
-
-}
