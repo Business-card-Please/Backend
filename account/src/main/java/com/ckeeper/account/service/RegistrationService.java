9ac1d0e1967aba4930d9cc9234323411023f4363
seophohoho <seop0937@gmail.com>
2024-08-09 22:02:35 +0900

    [feat:registration] Add password encrypt

--- account/src/main/java/com/ckeeper/account/service/RegistrationService.java
+++ account/src/main/java/com/ckeeper/account/service/RegistrationService.java
@@ -4,22 +4,29 @@ import com.ckeeper.account.dto.RegistrationRequest1;
 import com.ckeeper.account.entity.AccountEntity;
 import com.ckeeper.account.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
+
+    @Autowired
     private final AccountRepository accountRepository;
 
     @Autowired
-    public RegistrationService(AccountRepository accountRepository) {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(AccountRepository accountRepository,PasswordEncoder passwordEncoder) {
         this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void register1(RegistrationRequest1 registrationRequest1) {
         AccountEntity accountEntity = new AccountEntity();
         accountEntity.setEmail(registrationRequest1.getEmail());
         accountEntity.setCollegeName(registrationRequest1.getCollegeName());
-        accountEntity.setPassword(registrationRequest1.getPassword());
+        accountEntity.setPassword(passwordEncoder.encode(registrationRequest1.getPassword()));
         accountRepository.save(accountEntity);
     }
 }
