c2745929e0107b703ed9cdc4349ac10e888ccc9b
seophohoho <seop0937@gmail.com>
2024-09-01 16:16:52 +0900

    [feat:rentalboard] Inital settings

---
+++ rentalboard/src/test/java/com/ckeeper/rentalboard/RentalboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckeeper.rentalboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
