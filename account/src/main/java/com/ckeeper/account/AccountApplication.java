fbdabe3d0c6973bf1c927ef791bd7f2303e069eb
seophohoho <seop0937@gmail.com>
2024-09-26 22:21:41 +0900

    [feat:etc] Update git action 8

--- account/src/main/java/com/ckeeper/account/AccountApplication.java
+++ account/src/main/java/com/ckeeper/account/AccountApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-//
-//
-//
-//
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableCaching
 public class AccountApplication {
